1f44793d088af678b380060537aa045692c03b80
devmahin <mahinhowlader123@gmail.com>
2024-02-01 18:29:59 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let arr = ["mahin howlader", "adib howlader", "hasan howlader"]
+// for(let i=0; i < arr.length; i++){
+//     console.log(arr[i])
+// }
+// let arrayRev = arr.reverse()
+// for(let array of arr){
+//     console.log(array)
+// }
+// console.log(arrayRev)
+
+// let i = 0;
+// while(i< arr.length){
+//     console.log(arr[i])
+//     i++;
+// }
+
+
+// let num = [1,2,3,4,5,6,7]
+// // console.log(num.reverse())
+
+
+// let array = [];
+// for(let i = 0; i<num.length; i++){
+//    array.unshift(num[i])
+
+// }
+// console.log(array)
+
+
+
+let backAcc = [1,2,3,4,5,6,7];
+console.log(backAcc)
+let arrays = [];
+
+
+for(let i=backAcc.length -1; i >= 0; i--){
+    arrays.push(backAcc[i])
+}
+console.log(arrays)
