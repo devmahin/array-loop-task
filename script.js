80442a5344eb5dd3537f3a1c377d32831d119e43
devmahin <mahinhowlader123@gmail.com>
2024-02-01 20:21:58 +0600

    sort

--- script.js
+++ script.js
@@ -28,12 +28,30 @@ let arr = ["mahin howlader", "adib howlader", "hasan howlader"]
 
 
 
-let backAcc = [1,2,3,4,5,6,7];
-console.log(backAcc)
-let arrays = [];
+// let backAcc = [1,2,3,4,5,6,7];
+// console.log(backAcc)
+// let arrays = [];
 
 
-for(let i=backAcc.length -1; i >= 0; i--){
-    arrays.push(backAcc[i])
-}
-console.log(arrays)
+// for(let i=backAcc.length -1; i >= 0; i--){
+//     arrays.push(backAcc[i])
+// }
+// console.log(arrays)
+
+
+
+
+let sort = [99,1,2,3,4,5,6,7,8];
+console.log(sort.sort())
+
+
+let sorting = ["d","e","r","t"]
+console.log(sorting.sort())
+
+
+let sorts = [11,22,44,1,1111111111,2,3,4,6,7,8,9]
+// let sortingFun = sorts.sort(function(a,b){return a-b})
+let sortingFun = sorts.sort(function(a,b){return b -a})
+console.log(sortingFun)
+
+
